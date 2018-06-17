e20be0855c5dba9fdb6af61eae319ffff19b262c
Innocent <31923567+angwandi@users.noreply.github.com>
2018-06-17 14:51:58 +0100

    Add filter for EditText length and Scrolling for Add Book Activity

--- app/src/main/java/com/example/demad/inventoryapp1/EditorBookActivity.java
+++ app/src/main/java/com/example/demad/inventoryapp1/EditorBookActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,11 +24,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.demad.inventoryapp1.data.BookContract;
+
 /**
  * Allows user to create a new book or edit an existing one.
  */
 public class EditorBookActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
+    InputFilter inputFilter;
     /**
      * Identifier for the book data loader
      */
